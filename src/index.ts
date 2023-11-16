0e493ad779fcb2ce5ebcef279bb1336865b538f0
sceonn <yoou201@naver.com>
2023-11-16 22:36:42 +0900

    express and serverless offline

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express from 'express'
+import serverless from 'serverless-http'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.json('good')
-})
+app.get("/", (req, res, next) => {
+    return res.status(200).json({
+        message: "Hello from root!",
+    });
+});
 
-app.listen(1234)
+app.get("/path", (req, res, next) => {
+    return res.status(200).json({
+        message: "Hello from path!",
+    });
+});
+
+app.use((req, res, next) => {
+    return res.status(404).json({
+        error: "Not Found",
+    });
+});
+
+export const handler = serverless(app)
